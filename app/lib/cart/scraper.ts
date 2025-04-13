689869397ea715e62b188040ad163343068f03b2
Vibha! <110997540+vibiye@users.noreply.github.com>
2025-04-13 05:30:29 -0400

    scraper stuff commented out. wjo needs that anyway

--- app/lib/cart/scraper.ts
+++ app/lib/cart/scraper.ts
@@ -1,39 +1,39 @@
-// import puppeteer from 'puppeteer';
-// // import cheerio from 'cheerio';
+// // import puppeteer from 'puppeteer';
+// // // import cheerio from 'cheerio';
 
-// const cheerio = require('cheerio');
+// // const cheerio = require('cheerio');
 
-// const item = "milk"; //CHANGE THIS
+// // const item = "milk"; //CHANGE THIS
 
-// async function scrape() {
+// // async function scrape() {
 
-// // //   // Launch the browser and open a new blank page
-//   const browser = await puppeteer.launch({headless:false, slowMo:1000});
-//   const page = await browser.newPage();
+// // // //   // Launch the browser and open a new blank page
+// //   const browser = await puppeteer.launch({headless:false, slowMo:1000});
+// //   const page = await browser.newPage();
   
 
 // // //   // Navigate the page to a URL
 //   const url = 'https://www.instacart.com/store/s?k=' + item;
 //   await page.goto(url, {waitUntil: 'domcontentloaded'});
-
+//   await page.waitForSelector('span.e-6n7j50');
 // // //   // Set screen size
 //   await page.setViewport({width: 1080, height: 1024});
 //   const data = await page.content();
 //   await browser.close();
 //   return data;
 
-// //     console.log(data.match(/(\$[0-9]+\.[0-9]+)/g));
-// //       await page.evaluate(() => {
-// //     window.scrollTo(0, 3000);
-// //   });
-// //   const data2 = await page.content();
-// //     console.log(data2.match(/(\$[0-9]+\.[0-9]+)/g));
-// // //   // Type into search box
-// // // //   await page.type('.devsite-search-field', 'automate beyond recorder');
+// // //     console.log(data.match(/(\$[0-9]+\.[0-9]+)/g));
+// // //       await page.evaluate(() => {
+// // //     window.scrollTo(0, 3000);
+// // //   });
+// // //   const data2 = await page.content();
+// // //     console.log(data2.match(/(\$[0-9]+\.[0-9]+)/g));
+// // // //   // Type into search box
+// // // // //   await page.type('.devsite-search-field', 'automate beyond recorder');
 
 // // //   // Wait and click on first result
 // // //   const searchResultSelector = '.e-ecdxmw';
-// // //   await page.waitForSelector(searchResultSelector);
+// // //   
 // // //   console.log("meow");
 // // //   await page.click(searchResultSelector);
 // // //     console.log("mewmew")
@@ -41,27 +41,37 @@
 // // // //   // Locate the full title with a unique string
 
 
-// // //   console.log("teststy");
-// // //   // Check if network is idle after scrolling
-// // // //   await page.waitForNetworkIdle();
-// // // //   console.log("testsssst");
-// // //   const textSelector = await page.waitForSelector(
-// // //     '.e-8n9yrs'
-// // //   );
-// // //   console.log("testst")  
-// // //   const price = await textSelector?.evaluate(el => el.textContent);
-// // //   console.log(price);
+// // // //   console.log("teststy");
+// // // //   // Check if network is idle after scrolling
+// // // // //   await page.waitForNetworkIdle();
+// // // // //   console.log("testsssst");
+// // // //   const textSelector = await page.waitForSelector(
+// // // //     '.e-8n9yrs'
+// // // //   );
+// // // //   console.log("testst")  
+// // // //   const price = await textSelector?.evaluate(el => el.textContent);
+// // // //   console.log(price);
 
-// // // //   // Print the full title
-// // // //   console.log('Price is".', price);
+// // // // //   // Print the full title
+// // // // //   console.log('Price is".', price);
 
 
-// // })();
-// }
+// // // })();
+// // }
 
 // scrape().then((data) => {
 //   let arr = []
 //   let $ = cheerio.load(data);
-//   console.log($('li.e-14qbqkc').find('span.e-6gqwm0').text().trim());
-
-// })
+//   for(const element of $('li.e-14qbqkc span.e-6gqwm0')) {
+//     console.log($(element).text());
+//   }
+//   for(const element2 of $('span.e-6n7j50')) {
+//     console.log("hi")
+//     console.log($(element2).text());
+//   }
+//   // $('li.e-14qbqkc').map((elem: any) => {
+//   //   console.log("hi");
+//   //   // console.log($(elem).find('span.e-6gqwm0').text());
+//   // })
+// }
+// )
